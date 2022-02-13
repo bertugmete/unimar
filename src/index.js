d36198ce8fcb38f4d9edc5b48c7ddc5f9cc5246a
Bertuğ Mete <bertumete95@gmail.com>
2022-02-13 16:30:35 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.scss'
+
+const App = () => {
+  return <div>Home Page42233</div>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
+if (module.hot) {
+  module.hot.accept()
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin')
+
+const currentTask = process.env.npm_lifecycle_event
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[hash].js'
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  devServer: {
+    port: 8080,
+    static: path.join(__dirname, 'dist'),
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  useBuiltIns: 'usage',
+                  corejs: 3,
+                  targets: 'defaults'
+                }
+              ],
+              '@babel/preset-react'
+            ]
+          }
+        }
+      }
+    ]
+  }
+}
+
+if (currentTask === 'build') {
+  config.mode = 'production'
+  config.module.rules[0].use[0] = MiniCssExtractPlugin.loader
+  config.plugins.push(
+    new MiniCssExtractPlugin({
+      filename: 'main.[hash].css'
+    }),
+    new CleanWebpackPlugin(),
+    new WebpackManifestPlugin()
+  )
+}
+
+module.exports = config
